9d604e8adcff0a8338ae9ce7a4ffbad00e3348fd
MarulandaT <migue.marulanda99@gmail.com>
2019-02-12 20:56:01 -0500

    Iniciando Proyecto 2

---
+++ Proyecto/Proyecto2/src/BatallaNaval/Ejecutar.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package BatallaNaval;
+
+/**
+ *
+ * @author l_mar
+ */
+public class Ejecutar {
+    public static void main(String[] args){
+        new LogicaJuego().Iniciar();
+    }
+    
+}
